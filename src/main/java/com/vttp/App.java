afe04026f885d4ddb919531149fabf9160188fcc
hyewlim <hyew.lim@gmail.com>
2022-06-27 09:21:52 +0800

    first commit

---
+++ src/main/java/com/vttp/App.java
@@ -0,0 +1,13 @@
+package com.vttp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
